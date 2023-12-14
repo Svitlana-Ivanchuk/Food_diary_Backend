2f8a040afaa7b4d66282f0aac14b52088d220982
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-12-14 11:52:08 +0200

    update server

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const app = require("./app");
+const app = require('./app');
 
-const DB_URI = process.env.DB_URI;
+const { DB_URI, PORT } = process.env;
 
-mongoose.set("strictQuery", true);
+mongoose.set('strictQuery', true);
 
 mongoose
- .connect(DB_URI)
- .then(() => console.info("Database connection successful"))
- .catch((error) => {
-  console.error(error);
-  process.exit(1);
- });
+  .connect(DB_URI)
+  .then(() => {
+    console.info('Database connection successful');
+    app.listen(PORT, () =>
+      console.log('Server running. Use our API on port: 3000'),
+    );
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
 
-app.listen(3000, () => {
- console.log("Server running. Use our API on port: 3000");
-});
+//app.listen(3000, () => {
+//  console.log('Server running. Use our API on port: 3000');
+//});
