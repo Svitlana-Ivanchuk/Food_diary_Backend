e2a975bdf2560c6e522258486e2635e21f59cb3e
Aleksandr Borysenko <borysenko343@gmail.com>
2023-12-17 15:28:47 +0200

    CLOUDINARY

--- routers/api/users.js
+++ routers/api/users.js
@@ -3,10 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const ctrl = require('../../controllers/users');
-const { authenticate } = require('../../middlewares');
+const { authenticate, uploadCloud } = require('../../middlewares');
 
 router.get('/current', authenticate, ctrl.getCurrent);
 
-router.put('/update', authenticate, ctrl.updateUser);
+router.put(
+  '/update',
+  authenticate,
+  uploadCloud.single('avatarURL'),
+  ctrl.updateUser,
+);
 
 module.exports = router;
