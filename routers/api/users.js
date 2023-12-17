492aeb037f55067ee3aa445598dc93d0253d0a08
Hlukhenka <anastasiagluhenka@gmail.com>
2023-12-17 16:15:18 +0200

    add: deleteFood, addWater, deleteWater

--- routers/api/users.js
+++ routers/api/users.js
@@ -9,4 +9,14 @@ router.get('/current', authenticate, ctrl.getCurrent);
 
 router.put('/update', authenticate, ctrl.updateUser);
 
+router.post('/food-intake', authenticate, ctrl.addFood);
+
+router.put('/food-intake/:id', authenticate, ctrl.updateFood);
+
+router.delete('/food-intake/', authenticate, ctrl.deleteFood);
+
+router.post('/water-intake', authenticate, ctrl.addWater);
+
+router.delete('/water-intake/', authenticate, ctrl.deleteWater);
+
 module.exports = router;
