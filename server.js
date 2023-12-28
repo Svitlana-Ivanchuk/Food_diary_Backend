320191765be6217a00b573b5abb63a6df8e74d5a
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-12-28 13:57:03 +0200

    add in swagger delete/food-intake

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ mongoose.set('strictQuery', true);
 mongoose
   .connect(DB_URI)
   .then(() => {
-    console.info('Database connection successful');
     app.listen(PORT, () =>
       console.log('Server running. Use our API on port: 3000'),
     );
